e762a90a7d949c55e03aec11fc0d3b26f737b59b
dasilva <muluba@gmail.com>
2019-02-13 14:19:37 +0000

    updated mailer sandgrid

--- config/application.rb
+++ config/application.rb
@@ -16,19 +16,29 @@ module Tutanus
     # -- all .rb files in that directory are automatically loaded.
     #config.action_mailer.default_url_options = { :host => 'http://www.tutanus.com' }# For mail_form gem
     config.action_mailer.default_url_options = { :host => 'https://tutanus.herokuapp.com/'}
-    config.action_mailer.perform_deliveries = true
-    config.action_mailer.raise_delivery_errors = true
-    config.action_mailer.delivery_method = :smtp
+    #config.action_mailer.perform_deliveries = true
+    #config.action_mailer.raise_delivery_errors = true
+    #config.action_mailer.delivery_method = :smtp
 
-    config.action_mailer.smtp_settings = {
-      :address => "smtp.gmail.com",
-      :port => 587,
-      :domain => "gmail.com",
-      :user_name => "dasilva.is.design@gmail.com",
-      :password => "3kmdeaveiro",
-      :authentication => "plain",
-      :enable_starttls_auto => true
-  }
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+ address: 'smtp.sendgrid.net',
+ port: '25',
+ domain: 'heroku.com',
+ user_name: ENV['SENDGRID_USERNAME'],
+ password: ENV['SENDGRID_PASSWORD'],
+ authentication: 'plain',
+ enable_starttls_auto: true
+}
+   # config.action_mailer.smtp_settings = {
+   #   :address => "smtp.gmail.com",
+   #   :port => 587,
+   #   :domain => "gmail.com",
+   #   :user_name => "dasilva.is.design@gmail.com",
+   #   :password => "3kmdeaveiro",
+   #   :authentication => "plain",
+   #   :enable_starttls_auto => true
+  #}
 
   end
 end
