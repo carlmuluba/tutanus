a8945cc393df106dc04171f11df57f71cf2a68dc
dasilva <muluba@gmail.com>
2019-02-13 14:08:52 +0000

    updated mailer default url

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,8 @@ module Tutanus
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.action_mailer.default_url_options = { :host => 'http://www.tutanus.com' }# For mail_form gem
+    #config.action_mailer.default_url_options = { :host => 'http://www.tutanus.com' }# For mail_form gem
+    config.action_mailer.default_url_options = { :host => 'https://tutanus.herokuapp.com/'}
     config.action_mailer.perform_deliveries = true
     config.action_mailer.raise_delivery_errors = true
     config.action_mailer.delivery_method = :smtp
